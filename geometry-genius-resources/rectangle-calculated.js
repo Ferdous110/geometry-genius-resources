b9372fed4c18a7d616edf6c262e28d6ccfe556db
ANJUM FERDOUS <anjumferdous110@gmail.com>
2024-11-22 23:30:20 +0600

    Ractangle calcutate

--- geometry-genius-resources/rectangle-calculated.js
+++ geometry-genius-resources/rectangle-calculated.js
@@ -1,4 +1,23 @@
 
 function calculateRectangleArea() {
-   console.log('click')
- }
+  // get lenght of the rectangle
+  const lengthInput = document.getElementById("rectangle-length");
+  const lenghtText = lengthInput.value;
+  const lenght = parseFloat(lenghtText);
+
+  // get width oof the rectanle
+
+  const widthInput = document.getElementById("rectangle-width");
+  const widthText = widthInput.value;
+  const weidth = parseFloat(widthText);
+
+  //  display ractangle area
+  const area = lenght * weidth;
+
+  console.log(area);
+
+  // display ractangle area
+  
+  const areaDisplay = document.getElementById("ractangle-area");
+  areaDisplay.innerText = area;
+}
