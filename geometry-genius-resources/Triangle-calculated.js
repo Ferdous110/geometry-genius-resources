dc479c2348231b7b27a5f850c2076f5eda93484d
ANJUM FERDOUS <anjumferdous110@gmail.com>
2024-11-22 22:40:29 +0600

    Calculation triangle using javaScript

--- geometry-genius-resources/Triangle-calculated.js
+++ geometry-genius-resources/Triangle-calculated.js
@@ -1,3 +1,29 @@
+
+// get triangle base value 
+
 function calculateTriangleArea() {
-  console.log('inside function')
+  const triangleBaseInput = document.getElementById("triangle-base");
+  const triangleBaseText = triangleBaseInput.value;
+  const base = parseFloat(triangleBaseText);
+  console.log(base)
+
+  // get triangle height value
+
+  const triangleHightInput = document.getElementById("triangle-height");
+  const triangleHightText = triangleHightInput.value;
+  const height = parseFloat(triangleHightText);
+  console.log(height);
+
+  // calculate triangle area
+
+  const area = 0.5 * base * height;
+  console.log('area of the trianle is:', area);
+
+  // display triangle area
+  const triangleAreaSpan = document.getElementById("triangle-area");
+  triangleAreaSpan.innerText = area;
+
+
+
 }
+
